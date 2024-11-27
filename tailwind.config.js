12e27659278043a1f5d1d4797a1eecc1116aa0ad
verorodzn <a222215753@unison.mx>
2024-11-26 18:10:08 -0700

    Upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'bkg-primary': '#161616',
+        'bkg-mid': '#0F0F0F',
+        'bkg-light': '#242424',
+        'bkg-hover': '#2E2E2E', // Hover y borders
+        'border': '#292929',
+        'light-border': "#3B3B3B",
+        'blue-primary': '#78A7FF',
+        'light-blue-primary': '#78C4FF',
+        'orange-primary': '#FFB865',
+        'pink-primary': '#E6ACFF',
+        'yellow-primary': '#FFEF85',
+        'light': '#C3C5D4',
+
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
